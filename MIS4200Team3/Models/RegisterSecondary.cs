488ac5afc8312560486961a17d7785afed60cfa1
Ryan Wesolowski <rw103617@ohio.edu>
2021-10-13 11:47:06 -0400

    Secondary Register Page

---
+++ MIS4200Team3/Models/RegisterSecondary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team3.Models
+{
+    public class RegisterSecondary
+    {
+        [Key]
+        public int registerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string businessUnit { get; set; }
+        public DateTime hireDate { get; set; }
+        public string title { get; set; }
+    }
+}
