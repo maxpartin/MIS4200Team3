994e853e912b33e7228d7bf0489e40ee32bc3e60
Ross Ferraro <rf402317@ohio.edu>
2021-11-01 12:23:13 -0400

    woot

--- MIS4200Team3/Models/RegisterSecondary.cs
+++ MIS4200Team3/Models/RegisterSecondary.cs
@@ -10,10 +10,15 @@ namespace MIS4200Team3.Models
     {
         [Key]
         public int registerID { get; set; }
+        [Display(Name = "First Name")]
         public string firstName { get; set; }
+        [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Display(Name = "Business Unit")]
         public string businessUnit { get; set; }
+        [Display(Name = "Date Hired")]
         public DateTime hireDate { get; set; }
+        [Display(Name = "Title")]
         public string title { get; set; }
     }
 }
