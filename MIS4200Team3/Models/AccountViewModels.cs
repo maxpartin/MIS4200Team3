012917314a4158271eb281b352181105e4b17958
Ross Ferraro <rf402317@ohio.edu>
2021-12-01 11:41:46 -0500

    poosh

--- MIS4200Team3/Models/AccountViewModels.cs
+++ MIS4200Team3/Models/AccountViewModels.cs
@@ -80,23 +80,6 @@ namespace MIS4200Team3.Models
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
-        [Required]
-        [Display(Name = "First Name")]
-        public string firstName { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        public string lastName { get; set; }
-
-        [Required]
-        [Display(Name = "Business Unit")]
-        public string businessUnit { get; set; }
-
-        [Required]
-        [DataType(DataType.Date)]
-        [Display(Name = "Date Hired")]
-        public string dateHired { get; set; }
-
     }
 
     public class ResetPasswordViewModel
