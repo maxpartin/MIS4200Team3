22b98a4e5fc449281a8b8a8fa99d098174f3f656
Ross Ferraro <rf402317@ohio.edu>
2021-12-03 11:22:24 -0500

    woot

---
+++ MIS4200Team3/Controllers/AwardsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MIS4200Team3.DAL;
+using MIS4200Team3.Models;
+
+namespace MIS4200Team3.Controllers
+{
+    public class AwardsController : Controller
+    {
+        private RegisterContext db = new RegisterContext();
+
+        // GET: Awards
+        public ActionResult Index()
+        {
+            return View(db.Awards.ToList());
+        }
+
+        // GET: Awards/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Award award = db.Awards.Find(id);
+            if (award == null)
+            {
+                return HttpNotFound();
+            }
+            return View(award);
+        }
+
+        // GET: Awards/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Awards/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,award,recognizor,recognized,recognizationDate")] Award award)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Awards.Add(award);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(award);
+        }
+
+        // GET: Awards/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Award award = db.Awards.Find(id);
+            if (award == null)
+            {
+                return HttpNotFound();
+            }
+            return View(award);
+        }
+
+        // POST: Awards/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,award,recognizor,recognized,recognizationDate")] Award award)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(award).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(award);
+        }
+
+        // GET: Awards/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Award award = db.Awards.Find(id);
+            if (award == null)
+            {
+                return HttpNotFound();
+            }
+            return View(award);
+        }
+
+        // POST: Awards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Award award = db.Awards.Find(id);
+            db.Awards.Remove(award);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
