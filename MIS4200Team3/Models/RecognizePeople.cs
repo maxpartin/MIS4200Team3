8e6e4be064dbd84113a25328325ec671aafe85b9
Ross Ferraro <rf402317@ohio.edu>
2021-12-05 23:32:35 -0500

    Naming and Recognize/Awards

--- MIS4200Team3/Models/RecognizePeople.cs
+++ MIS4200Team3/Models/RecognizePeople.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,17 @@ namespace MIS4200Team3.Models
 {
     public class RecognizePeople
     {
-        public int RecognizePeopleID { get; set; }
-        public string recognizerFirstName { get; set; }
-        public string recognizerLastName { get; set; }
+        
+        public int RecognizePeopleID { get; set; } 
+    [Display(Name =  "Recognizer First Name")]
+        public string recognizerFirstName { get; set; } 
+        [Display(Name = "Recognizer Last Name")]
+        public string recognizerLastName { get; set; } 
+        [Display(Name = "Recognizee First Name")]
         public string recognizeeFirstName { get; set; }
-        public string recognizeeLastName { get; set; }
+        [Display(Name = "Recognizee Last Name")]
+        public string recognizeeLastName { get; set; } 
+
         public string Values { get; set; }
     }
 }
